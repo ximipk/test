8123ec30f001f08c26cadbf8ab9b66a6f8461167
ximi <623859503@qq.com>
2018-08-19 20:07:29 +0800

    test

--- input_game2.py
+++
@@ -1,9 +0,0 @@
-
-temp = input("请输入数字：");
-guess = int(temp);
-asd=0;
-while (int(guess)!=8):
-    asd = asd+1;
-    guess = input("输入错误,请重新输入数字：");
-
-print("游戏结束");
--- random01.py
+++
@@ -1,3 +0,0 @@
-import random
-test_i = random.randint(1,10)
-print(test_i);
